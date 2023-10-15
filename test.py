a5261cd3e92be269fea85b9894d71f8b5954b57d
Jaspar Martin <jasparmartin@gmail.com>
2023-10-15 22:37:19 +1100

    i am good

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-from playsound import playsound
-playsound('01_Nathan_Statement.mp3')
+#from playsound import playsound
+#playsound('01_Nathan_Statement.mp3')
 
-'''
 from pydub import AudioSegment
 from pydub.playback import play
 
-song = AudioSegment.from_mp3("01_Nathan_Statement.mp3")
-play(song)
-'''
+#song = AudioSegment.from_mp3("01_Nathan_Statement.mp3")
+#play(song)
+
+while True:
+    sound = input("Type something: ")
+    try:
+        play(AudioSegment.from_mp3((sound + ".mp3")))
+    except:
+        print("Uh no")

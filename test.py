e16c75ae232a77d7ddd005da41bfd5d7ab5976ee
jasparm <jasparmartin@gmail.com>
2023-10-15 19:54:37 +1100

    added sound playing and a file

---
+++ test.py
@@ -0,0 +1,10 @@
+from playsound import playsound
+playsound('01_Nathan_Statement.mp3')
+
+'''
+from pydub import AudioSegment
+from pydub.playback import play
+
+song = AudioSegment.from_mp3("01_Nathan_Statement.mp3")
+play(song)
+'''
